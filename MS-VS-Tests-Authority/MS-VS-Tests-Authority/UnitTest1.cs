05cb40b77fa5e6c17fc4792ae6d2fce1e3743428
Katrina L <Strong_Girl_666@hotmail.com>
2018-09-06 17:45:41 +0300

    Init

---
+++ MS-VS-Tests-Authority/MS-VS-Tests-Authority/UnitTest1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace SeleniumTests
+{
+    [TestFixture]
+    public class Untitled
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+
+          driver = new ChromeDriver();
+          baseURL = "https://local.authoritycrm.com/";
+
+            // System.setProperty("webdriver.chrome.driver", @"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe");
+
+            /*
+               FirefoxOptions options = new FirefoxOptions();
+               options.BrowserExecutableLocation = @"d:\Program Files\Mozilla Firefox\firefox.exe";
+               options.UseLegacyImplementation = true;
+               driver = new FirefoxDriver(options);
+               baseURL = "https://local.authoritycrm.com/";
+               verificationErrors = new StringBuilder(); */
+
+        }
+
+     
+
+        [Test]
+        public void TheUntitledTest()
+        {
+            driver.Navigate().GoToUrl(baseURL + "dev/default.aspx?subj=doors/login");
+            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_UserName")).Clear();
+            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_UserName")).SendKeys("katerina");
+            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_Password")).Clear();
+            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_Password")).SendKeys("Lkaterina1234");
+            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_LoginButton")).Click();
+            driver.FindElement(By.Id("KaterinaLatyshevaMainMenu")).Click();
+            driver.FindElement(By.Id("LogoutMainMenu")).Click();
+        }
+
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+          Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
