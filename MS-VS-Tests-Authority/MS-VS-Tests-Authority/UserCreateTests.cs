e927fa447cf0a4c476798b3550b5cb8bc2680d1c
Katrina L <Strong_Girl_666@hotmail.com>
2018-09-20 16:56:45 +0300

    Step by step it works. running - no.

--- MS-VS-Tests-Authority/MS-VS-Tests-Authority/UserCreateTests.cs
+++ MS-VS-Tests-Authority/MS-VS-Tests-Authority/UserCreateTests.cs
@@ -19,22 +19,24 @@ namespace WebAuthorityTests
         private string baseURL;
         private bool acceptNextAlert = true;
 
-        
-
         [SetUp]
         public void SetupTest()
         {
-            
             driver = new ChromeDriver(@"C:\Windows\SysWOW64");
             baseURL = "https://local.authoritycrm.com/";
             driver.Manage().Window.Maximize();
         }
 
+        public IWebElement WaitUntilElementIsFound(string name)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement myDynamicElement = wait.Until<IWebElement>(d => d.FindElement(By.Id(name)));
+            return myDynamicElement;
+        }
+
         [Test]
         public void UserCreateTest()
         {
-
-           
             driver.Navigate().GoToUrl(baseURL + "dev/default.aspx?subj=doors/login");
             driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_UserName")).Clear();
             driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_UserName")).SendKeys("katerina");
@@ -43,9 +45,16 @@ namespace WebAuthorityTests
             driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_LoginButton")).Click();
             driver.FindElement(By.Id("SystemMainMenu")).Click();
             driver.FindElement(By.Id("UsersMainMenu")).Click();
-            System.Threading.Thread.Sleep(1000);
+
+            WaitUntilElementIsFound("NewBtn");
             driver.FindElement(By.Id("NewBtn")).Click();
+
+            //var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            //wait.Until(d => d.FindElement(By.Id("NewBtn")));
+            //System.Threading.Thread.Sleep(1000);
+
             driver.FindElement(By.Id("Login")).Clear();
+
             driver.FindElement(By.Id("Login")).SendKeys("Tiger");
             driver.FindElement(By.Id("Password")).Clear();
             driver.FindElement(By.Id("Password")).SendKeys("666");
@@ -58,11 +67,12 @@ namespace WebAuthorityTests
             driver.FindElement(By.Id("Email")).Clear();
             driver.FindElement(By.Id("Email")).SendKeys("white@tiger.com");
             driver.FindElement(By.Id("SubmitBtn")).Click();
-            System.Threading.Thread.Sleep(1000);
+     
+            WaitUntilElementIsFound("KaterinaLatyshevaMainMenu");
+                        
             driver.FindElement(By.Id("KaterinaLatyshevaMainMenu")).Click();
             driver.FindElement(By.Id("LogoutMainMenu")).Click();
         }
-
 
         [TearDown]
         public void TeardownTest()
