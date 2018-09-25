a0a28619a037431ea2fbee4c943ce39037d1abca
Katrina L <Strong_Girl_666@hotmail.com>
2018-09-25 17:27:23 +0300

    Admin logs In, creates User and Logs out.

--- MS-VS-Tests-Authority/MS-VS-Tests-Authority/UserCreateTests.cs
+++ MS-VS-Tests-Authority/MS-VS-Tests-Authority/UserCreateTests.cs
@@ -3,14 +3,13 @@ using System.Text;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
+using System.Collections.Generic;
 using OpenQA.Selenium.Support.UI;
 
 
 namespace WebAuthorityTests
 {
     [TestFixture]
-
 
     public class UserCreateTests
     {
@@ -26,54 +25,96 @@ namespace WebAuthorityTests
             baseURL = "https://local.authoritycrm.com/";
             driver.Manage().Window.Maximize();
         }
+        
+        [Test]
+        public void UserCreationTest()
+        {
+            OpenLoginPage();
+            Login(new LoginData("admin", "gbd4fycr0t"));
+            GoToUserList();
+            WaitUntilSpinnerVisible();
+            InitNewUserCreation();
+            UserDetails user = new UserDetails("Lemon");
+            user.Password = "12345";
+            user.Passwordconfirm = "12345";
+            user.Firstname = "Limon";
+            user.Lastname = "Limonych";
+            user.Email = "limon@lime.com";
+            FillNewUserForm(user);
+            SubmitUserCreation();
+            WaitUntilSpinnerVisible();
+           // WaitUntilElementIsFound("AdministratorMainMenu");
+            InitLogOut();
+        }
+
+        private void InitLogOut()
+        {
+            driver.FindElement(By.Id("AdministratorMainMenu")).Click();
+            driver.FindElement(By.Id("LogoutMainMenu")).Click();
+        }
+
+        private void SubmitUserCreation()
+        {
+            driver.FindElement(By.Id("SubmitBtn")).Click();
+        }
+
+        private void FillNewUserForm(UserDetails user)
+        {
+            driver.FindElement(By.Id("Login")).Clear();
+            driver.FindElement(By.Id("Login")).SendKeys(user.Login);
+            driver.FindElement(By.Id("Password")).Clear();
+            driver.FindElement(By.Id("Password")).SendKeys(user.Password);
+            driver.FindElement(By.Id("PasswordConfirm")).Clear();
+            driver.FindElement(By.Id("PasswordConfirm")).SendKeys(user.Passwordconfirm);
+            driver.FindElement(By.Id("FirstName")).Clear();
+            driver.FindElement(By.Id("FirstName")).SendKeys(user.Firstname);
+            driver.FindElement(By.Id("LastName")).Clear();
+            driver.FindElement(By.Id("LastName")).SendKeys(user.Lastname);
+            driver.FindElement(By.Id("Email")).Clear();
+            driver.FindElement(By.Id("Email")).SendKeys(user.Email);
+        }
+
+        private void InitNewUserCreation()
+        {
+            driver.FindElement(By.Id("NewBtn")).Click();
+        }
+
+        private void GoToUserList()
+        {
+            driver.FindElement(By.Id("SystemMainMenu")).Click();
+            driver.FindElement(By.Id("UsersMainMenu")).Click();
+        }
+
+        private void Login(LoginData account)
+        {
+            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_UserName")).Clear();
+            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_UserName")).SendKeys(account.Username);
+            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_Password")).Clear();
+            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_Password")).SendKeys(account.Password);
+            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_LoginButton")).Click();
+        }
+
+        
+        private void OpenLoginPage()
+        {
+            driver.Navigate().GoToUrl(baseURL + "dev/default.aspx?subj=doors/login");
+        }
 
         public IWebElement WaitUntilElementIsFound(string name)
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(100));
             IWebElement myDynamicElement = wait.Until<IWebElement>(d => d.FindElement(By.Id(name)));
             return myDynamicElement;
         }
 
-        [Test]
-        public void UserCreateTest()
+        public void WaitUntilSpinnerVisible()
         {
-            driver.Navigate().GoToUrl(baseURL + "dev/default.aspx?subj=doors/login");
-            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_UserName")).Clear();
-            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_UserName")).SendKeys("katerina");
-            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_Password")).Clear();
-            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_Password")).SendKeys("Lkaterina1234");
-            driver.FindElement(By.Id("ctl00_BodyPlaceholder_LoginLayoutLoader_ctl00_ContentLoader_ctl00_LoginButton")).Click();
-            driver.FindElement(By.Id("SystemMainMenu")).Click();
-            driver.FindElement(By.Id("UsersMainMenu")).Click();
-
-            WaitUntilElementIsFound("NewBtn");
-            driver.FindElement(By.Id("NewBtn")).Click();
-
-            //var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            //wait.Until(d => d.FindElement(By.Id("NewBtn")));
-            //System.Threading.Thread.Sleep(1000);
-
-            driver.FindElement(By.Id("Login")).Clear();
-
-            driver.FindElement(By.Id("Login")).SendKeys("Tiger");
-            driver.FindElement(By.Id("Password")).Clear();
-            driver.FindElement(By.Id("Password")).SendKeys("666");
-            driver.FindElement(By.Id("PasswordConfirm")).Clear();
-            driver.FindElement(By.Id("PasswordConfirm")).SendKeys("666");
-            driver.FindElement(By.Id("FirstName")).Clear();
-            driver.FindElement(By.Id("FirstName")).SendKeys("White");
-            driver.FindElement(By.Id("LastName")).Clear();
-            driver.FindElement(By.Id("LastName")).SendKeys("Tiger");
-            driver.FindElement(By.Id("Email")).Clear();
-            driver.FindElement(By.Id("Email")).SendKeys("white@tiger.com");
-            driver.FindElement(By.Id("SubmitBtn")).Click();
-     
-            WaitUntilElementIsFound("KaterinaLatyshevaMainMenu");
-                        
-            driver.FindElement(By.Id("KaterinaLatyshevaMainMenu")).Click();
-            driver.FindElement(By.Id("LogoutMainMenu")).Click();
-        }
-
+            ICollection<IWebElement> Collection;
+            do
+            {
+                Collection = driver.FindElements(By.ClassName("loader-wrapper"));
+            } while (Collection.Count > 0);
+        }
         [TearDown]
         public void TeardownTest()
         {
---
+++ MS-VS-Tests-Authority/MS-VS-Tests-Authority/UserData.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAuthorityTests
+{
+    class UserDetails
+    {
+
+        private string login;
+        private string password;
+        private string passwordconfirm;
+        private string firstname;
+        private string lastname;
+        private string email;
+
+        public UserDetails(string login, string password, string passwordconfirm, string firstname, string lastname, string email)
+        {
+            this.login = login;
+            this.password = password;
+            this.passwordconfirm = passwordconfirm;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.email = email;
+        }
+
+        public UserDetails(string login)
+        {
+            this.login = login;
+        }
+
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+            set
+            {
+                login = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+            }
+        }
+
+        public string Passwordconfirm
+        {
+            get
+            {
+                return passwordconfirm;
+            }
+            set
+            {
+                passwordconfirm = value;
+            }
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+    }
+}
